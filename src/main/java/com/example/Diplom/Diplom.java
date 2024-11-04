0383f265fd1f38dbb7714561b0e879d13e780cb1
Aleksey <lexx29@yandex.ru>
2024-11-04 22:48:32 +0300

    first commit 1

--- src/main/java/com/example/Diplom/Diplom.java
+++ src/main/java/com/example/Diplom/Diplom.java
@@ -1,11 +1,12 @@
 package com.example.Diplom;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Diplom {
 
+public class Diplom {
 	public static void main(String[] args) {
 		SpringApplication.run(Diplom.class, args);
 	}
